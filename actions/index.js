e48a78050d4755b20110d111ed0c1d0d5680367a
ghaash <daniel.silverman@me.com>
2017-03-22 09:37:52 -0700

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,17 @@
+const React = require('react');
+const CounterStore = require('../stores/counterStore')
+
+const increment = () =>  {
+  CounterStore.increment();
+};
+
+const decrement = () => {
+  CounterStore.decrement();
+};
+
 module.exports = {
   // Your implementation here.
   // Hint: We need two actions!
+  increment,
+  decrement
 };
